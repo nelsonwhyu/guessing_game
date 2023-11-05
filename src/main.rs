57da582777cbcc99ca12f2b6cfb92c8a618486e6
Nelson Yu <nelsonwhyu@gmail.com>
2023-11-05 17:33:50 -0500

    minimal viable production code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    let mut count: u32 = 0;
+    println!("Guess the number!");
+
+    loop {
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please type a number!");
+                continue;
+            }
+        };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => {
+                count += 1;
+                println!("Too small!");
+            }
+            Ordering::Greater => {
+                count += 1;
+                println!("Too big!")
+            }
+            Ordering::Equal => {
+                count += 1;
+                println!("You win! It took {count} guesses.");
+                break;
+            }
+        }
+    }
 }
